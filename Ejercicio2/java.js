6b4b39e68505ad9f7bde60ef8dfd675ea53a46cb
Valery030905 <ovallecanon9@gmail.com>
2025-02-28 21:24:41 -0500

    Update(Tarjeta): Implementamos funcionalidad en el botón para poder cambiar un mensaje

--- Ejercicio2/java.js
+++ Ejercicio2/java.js
@@ -2,3 +2,7 @@ window.addEventListener("load",function(){
     document.body.classList.add("loaded");
     document.querySelector('.fade-in').classList.add('loaded');
 })
+
+function cambiarTexto() {
+    document.getElementById("cambioMensaje").innerText = "🌷¡La mejor Ingeniera de Sistemas!🌷";
+}
